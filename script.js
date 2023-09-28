e71c17739708dd40743e8cd2fcd381a497634b38
parallelbasket <a.milevskiy111@gmail.com>
2023-09-29 09:38:31 +1000

    Add button which changes the pixel width of the pad

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+function sketch() {
 const PADWIDTH = 720; // pixels
+let containerWidth = 16;
+const pixelContainer = document.querySelector('.pixel-container');
 
-function padSetup() {
-    let containerWidth = 16;
-    const pixelContainer = document.querySelector('.pixel-container');
+function setupPad() {        
     for (let i = 0; i < (containerWidth * containerWidth); i++) {
         const div = document.createElement('div');
         div.classList.add('pixel');
@@ -20,4 +21,39 @@ function updatePixelColor(e) {
     }
 }
 
-padSetup();
+function removePixels() {
+    const pixels = document.querySelectorAll('.pixel');
+    pixels.forEach(pixel => {
+        pixel.remove();
+    });
+}
+
+function getWidth() {
+    containerWidth = prompt("Update 'Etch-a-Sketch' pixel width:");
+    if (containerWidth > 100) {
+        alert('Max pixel width is 100');
+        getWidth();
+    }
+}
+
+function updatePixelWidth() {
+    let pixelSize = 720 / containerWidth;
+    const pixels = document.querySelectorAll('.pixel');
+    pixels.forEach(pixel => {
+        pixel.style.width = pixelSize.toString() + 'px';
+        pixel.style.height = pixelSize.toString() + 'px';
+    });
+}
+
+function changePadSize() {
+    const button = document.querySelector('button');
+    button.addEventListener('click', getWidth);
+    button.addEventListener('click', removePixels);
+    button.addEventListener('click', setupPad);
+    button.addEventListener('click', updatePixelWidth);
+}
+
+setupPad();
+changePadSize();
+}
+sketch();
